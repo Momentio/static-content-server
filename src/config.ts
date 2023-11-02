85608a2b189c1de6c02cd6a0085cc7e1f3a20d3b
Momentio <sergey.s.lebedev.russia@mail.ru>
2023-11-02 04:42:33 +0400

    feat: export

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export const config = {
     user: process.env.CONTENT_SERVER_USER || 'client',
     password: process.env.CONTENT_SERVER_PASSWORD || 'password',
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.CONTENT_SERVER_PORT) || 3000,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import fs from 'fs';
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
 import {v4} from 'uuid';
 import cors from 'cors';
 import * as basicAuth from 'express-basic-auth';
+import AdmZip from 'adm-zip';
+import bodyParser from 'body-parser';
 
 import {config} from './config';
 
@@ -24,11 +27,35 @@ const upload = multer({storage});
 
 const expressApp = express();
 
+expressApp.use(bodyParser.json());
 expressApp.use(cors());
 
 const authMiddleware = basicAuth.default({users: {[user]: password}});
 
 expressApp.use('/', express.static(buildPath));
+
+expressApp.post('/export', authMiddleware, (req, res) => {
+    fs.readdir(buildPath, (err, files) => {
+        const targetFiles = req.body?.files;
+
+        if (targetFiles && Array.isArray(targetFiles)) {
+            files = targetFiles.filter(v => files.includes(v));
+        }
+        
+        const zip = new AdmZip();
+
+        for (const file of files) {
+            zip.addLocalFile(path.join(buildPath, file));
+        }
+
+        res.writeHead(200, {
+            'Content-Disposition': `attachment; filename="content.zip"`,
+            'Content-Type': 'application/zip',
+        });
+
+        res.end(zip.toBuffer());
+    });
+});
 
 expressApp.post('/', authMiddleware, upload.array('data'), (req, res) => {
     const files = req.files as Express.Multer.File[];
